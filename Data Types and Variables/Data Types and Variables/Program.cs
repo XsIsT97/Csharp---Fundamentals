f0f5ce8d96c2c8076988b97bf349abc1a247d30d
XsIsT97 <galin_3xg@abv.bg>
2022-04-12 16:23:55 +0300

    Add Files

---
+++ Data Types and Variables/Data Types and Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Types_and_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo");
+        }
+    }
+}
---
+++ Data Types and Variables/E01.Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.ConvertMetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double kilometeres = meters / 1000.0;
+            Console.WriteLine($"{Math.Round(kilometeres, 2, MidpointRounding.AwayFromZero):F2}");
+        }
+    }
+}
