460f39acd7474ca908a500e2dc99648e9684fd6a
XsIsT97 <galin_3xg@abv.bg>
2022-04-14 23:28:24 +0300

    Add Files

---
+++ Objects and Classes/1. Randomize Words/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(0, words.Length);
+                
+                string currentWord = words[i];
+                words[i] = words[randomIndex];
+                words[randomIndex] = currentWord;
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            //Console.WriteLine(random.Next(1, 10));
+        }
+    }
+}
