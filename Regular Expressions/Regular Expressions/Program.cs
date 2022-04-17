bd354a4f9cdebcc76314808c626cbdb8abeb732d
XsIsT97 <galin_3xg@abv.bg>
2022-04-17 20:49:27 +0300

    Add Files

---
+++ Regular Expressions/Regular Expressions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Regular_Expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
