3c9ec3a60b528e45a89e1d099d82c7252270d078
XsIsT97 <galin_3xg@abv.bg>
2022-05-03 21:22:03 +0300

    Add New Files

---
+++ Exercises/E06.SoftUniBarIncome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SoftUniBarIncome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^\%(?<name>[A-Z][a-z]+)\%[^\|\%\.\$]*?\<(?<product>\w+)\>[^\|\%\.\$]*?\|(?<quantity>\d+)\|[^|%.$]*?(?<price>\d+(\.\d+)?)\$";
+
+            decimal income = 0m;
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end of shift")
+            {
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    string product = match.Groups["product"].Value;
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+
+                    decimal totalPrice = price * quantity;
+                    income += totalPrice;
+
+                    Console.WriteLine($"{name}: {product} - {totalPrice:f2}");
+                }
+            }
+
+            Console.WriteLine($"Total income: {income:f2}");
+        }
+    }
+}
---
+++ Exercises/E07.Race/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> participants = new Dictionary<string, int>();
+
+            string[] names = Console.ReadLine().Split(", ").ToArray();
+
+            foreach (string name in names)
+            {
+                participants.Add(name, 0);
+            }
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end of race")
+            {
+                string name = GetName(input);
+                int distance = GetDistance(input);
+
+                if (participants.ContainsKey(name))
+                {
+                    participants[name] += distance;
+                }
+            }
+
+            string[] ranking = participants
+                .OrderByDescending(x => x.Value)
+                .Take(3)
+                .Select(x => x.Key)
+                .ToArray();
+
+            Console.WriteLine($"1st place: {ranking[0]}\n2nd place: {ranking[1]}\n3rd place: {ranking[2]}");
+        }
+
+        static string GetName(string input)
+        {
+            string pattern = @"[A-Za-z]+";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            StringBuilder name = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                name.Append(match.Value);
+            }
+
+            return name.ToString();
+        }
+
+        static int GetDistance(string input)
+        {
+            string pattern = @"\d";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            int distance = 0;
+
+            foreach (Match match in matches)
+            {
+                distance += int.Parse(match.Value);
+            }
+
+            return distance;
+        }
+    }
+}
