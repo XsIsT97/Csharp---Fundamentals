1b1f2cea0a522dadacf163c6f5c130ecb6f29945
Galin <galin_3xg@abv.bg>
2023-11-16 13:39:02 +0200

    Some home work

---
+++ Home Work/Fun work13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fun_work13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int newI = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+                newI += i;
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Sum: {newI}");
+        }
+    }
+}
