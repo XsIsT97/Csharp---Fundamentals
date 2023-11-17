c77eeba6d98480d65f60c6455cf6dfc3d3d92a9f
Galin <galin_3xg@abv.bg>
2023-11-17 11:59:18 +0200

    Some home work

---
+++ Home Work/Fun work18/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Fun_work18
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string lastDigitSpelling = GetLastDigitSpelling(number);
+
+            Console.WriteLine($"{lastDigitSpelling}");
+        }
+
+        // Method to get the English spelling of the last digit
+        static string GetLastDigitSpelling(int num)
+        {
+            int lastDigit = Math.Abs(num % 10); // Ensure positive last digit
+
+            switch (lastDigit)
+            {
+                case 0:
+                    return "zero";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                default:
+                    return ""; // Should not reach here
+            }
+        }
+    }
+}
