571746839bd381a3e0b26bf9e76e14b35a819032
Galin <galin_3xg@abv.bg>
2023-11-17 11:52:25 +0200

    Some home work

---
+++ Home Work/Fun work17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Fun_work17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+
+            // Sort the numbers in descending order
+            if (num1 < num2)
+            {
+                double temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+
+            if (num1 < num3)
+            {
+                double temp = num1;
+                num1 = num3;
+                num3 = temp; ;
+            }
+
+            if (num2 < num3)
+            {
+                double temp = num2;
+                num2 = num3;
+                num3 = temp;
+            }
+
+            // Print the sorted numbers
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+
+        }
+    }
+}
