5e2a835b9885188e8427c6a72363341e6d7d46e5
Galin <galin_3xg@abv.bg>
2023-11-17 14:02:26 +0200

    Some home work didn't work

---
+++ Home Work/Fun work20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fun_work20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string reversedString = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversedString += input[i];
+            }
+            Console.WriteLine($"{reversedString}");
+        }
+    }
+}
