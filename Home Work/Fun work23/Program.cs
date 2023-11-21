b9e1caeaf896d91add5fd9fe4cdf7a4d2a77ffc9
Galin <galin_3xg@abv.bg>
2023-11-21 11:19:21 +0200

    Some home work

---
+++ Home Work/Fun work23/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fun_work23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the distance in meters:");
+
+            if (int.TryParse(Console.ReadLine(), out int distanceInMeters))
+            {
+                double distanceInKilometers = distanceInMeters / 1000.0;
+
+                Console.WriteLine($"Distance in kilometers: {distanceInKilometers:F2} km");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
