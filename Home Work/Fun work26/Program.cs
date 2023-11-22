00f8104e44f124293fbe76340df65bb06b3e46e5
Galin <galin_3xg@abv.bg>
2023-11-22 11:10:09 +0200

    Some home work

---
+++ Home Work/Fun work26/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fun_work26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
