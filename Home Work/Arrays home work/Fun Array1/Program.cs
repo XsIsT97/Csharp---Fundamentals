29afd1ebc2de0cf235d816c6482f7bbf636ca1ca
Galin <galin_3xg@abv.bg>
2023-11-22 17:04:51 +0200

    Some home work

---
+++ Home Work/Arrays home work/Fun Array1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fun_Array1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOFNUmbers = int.Parse(Console.ReadLine());
+            int[] items = new int[numberOFNUmbers];
+
+            for (int i = 0; i < numberOFNUmbers; i++)
+            {
+                items[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = items.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{items[i]} ");
+            }
+        }
+    }
+}
