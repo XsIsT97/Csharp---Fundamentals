e60fe3b1755c089e61a00ad043960556de7aed1c
Galin <galin_3xg@abv.bg>
2023-11-22 16:30:20 +0200

    Some home work

---
+++ Home Work/Arrays home work/Fun Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fun_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayNumber = int.Parse(Console.ReadLine());
+
+            string[] daysOfTheWeek = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if (dayNumber >= 1 && dayNumber <= daysOfTheWeek.Length)
+            {
+                Console.WriteLine(daysOfTheWeek[dayNumber]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
