c14d7805fb1fd0030f1f6c94ccc033b682482570
Galin <galin_3xg@abv.bg>
2023-11-24 15:47:31 +0200

    Some home work

---
+++ Home Work/Arrays home work/Fun Array8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Fun_Array8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int evenNumbers = 0;
+            int oddNumbers = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    evenNumbers += currentNumber;
+                }
+                else
+                {
+                    oddNumbers += currentNumber;
+                }
+            }
+            int newSum = evenNumbers - oddNumbers;
+            Console.WriteLine(newSum);
+
+        }
+    }
+}
---
+++ Home Work/Arrays home work/Fun Array9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Fun_Array9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] array2 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+            bool arraysAreDiffrent = false;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sum += array2[i];
+                if (array1[i] != array2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    arraysAreDiffrent = true;
+                    break;
+                }
+            }
+            if (!arraysAreDiffrent)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
