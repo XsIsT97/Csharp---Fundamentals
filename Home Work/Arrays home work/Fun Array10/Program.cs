74f3d7634104eebbdbb3cff6af0d1e9d34bdf1f4
Galin <galin_3xg@abv.bg>
2023-11-24 15:47:47 +0200

    Some home work

---
+++ Home Work/Arrays home work/Fun Array10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Fun_Array10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstLenght = array.Length;
+
+            for (int i = 0; i < firstLenght - 1; i++)
+            {
+                int[] condens = new int[array.Length - 1];
+
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    condens[j] = array[j] + array[j + 1];
+                }
+                array = condens;
+            }
+            Console.WriteLine(array[0]);
+        }
+    }
+}
