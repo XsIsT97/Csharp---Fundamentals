6033049989bae80c68b37deb8ae71dcfbef7fa4a
Galin <galin_3xg@abv.bg>
2023-11-24 16:30:56 +0200

    Some home work

---
+++ Arrays/Arrays Exercise/6. EqualSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fun_Array13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
