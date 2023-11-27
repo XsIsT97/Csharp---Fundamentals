0b869643d4b631f0e3241f0bab7cc4fd426960c6
Galin <galin_3xg@abv.bg>
2023-11-27 11:44:26 +0200

    add Files

---
+++ Data Types and Variables/Data Types and Variables-Lab/E10.Lower or Upper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10.LowerToUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char character = char.Parse(Console.ReadLine());
+
+            if (char.IsUpper(character))
+            {
+                Console.WriteLine("upper-case");
+            }
+
+            if (char.IsLower(character))
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
