02dd6ad8ade10095ee421a643344294b82d5d52d
Galin <galin_3xg@abv.bg>
2023-11-29 16:50:45 +0200

    Fix some problems

--- Methods/Methods-Exercise/03. Characters in Range/Program.cs
+++ Methods/Methods-Exercise/03. Characters in Range/Program.cs
@@ -6,29 +6,33 @@ namespace _03._Characters_in_Range
     {
         static void Main(string[] args)
         {
-            string firstString = Console.ReadLine();
-            string secondString = Console.ReadLine();
 
-            PrintCharactersBetween(firstString, secondString);
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+
+            PrintCharInRange(start, end);
+
         }
-        static void PrintCharactersBetween(char str1, char str2)
+
+        static void PrintCharInRange(char start, char end)
         {
-            char char1 = str1[0];
-            char char2 = str2[0];
-            // Swap characters if needed
-            if (char1 > char2)
+            if (start < end)
             {
-                char temp = char1;
-                char1 = char2;
-                char2 = temp;
+                for (int i = start + 1; i < end; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+
+            }
+            else
+            {
+                for (int i = end + 1; i < start; i++)
+                {
+                    Console.Write($"{(char)i} ");
+                }
+
             }
 
-            for (char i = (char)(char1 + 1); i < char2; i++)
-            {
-                Console.Write(i + " ");
-            }
-
-            Console.WriteLine();
         }
     }
 }
