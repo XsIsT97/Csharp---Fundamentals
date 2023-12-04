bb1a4ee1c2007df2f8db4c063e5b1b2a3242b0a3
Galin <galin_3xg@abv.bg>
2023-12-04 14:17:49 +0200

    Add Files

---
+++ 02.Data Types and Variables/Data Types and Variables-Exercises/11.SnowBalls/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11.SnowBalls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int snowballSnow = 0;
+            int snowballTime = 0;
+            int snowballQuality = 0;
+            BigInteger maxSnowballValue = 0;
+            int maxSnowballSnow = 0;
+            int maxSnowballTime = 0;
+            int maxSnowballQuality = 0;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                snowballSnow = int.Parse(Console.ReadLine());
+                snowballTime = int.Parse(Console.ReadLine());
+                snowballQuality = int.Parse(Console.ReadLine());
+                BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+                if (snowballValue > maxSnowballValue)
+                {
+                    maxSnowballValue = snowballValue;
+                    maxSnowballSnow = snowballSnow;
+                    maxSnowballTime = snowballTime;
+                    maxSnowballQuality = snowballQuality;
+                }
+
+            }
+            Console.WriteLine($"{maxSnowballSnow} : {maxSnowballTime} = {maxSnowballValue} ({maxSnowballQuality})");
+        }
+    }
+}
