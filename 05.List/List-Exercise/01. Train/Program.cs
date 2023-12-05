31e412615ac08a6a192aafcf5b65461cead5793f
Galin <galin_3xg@abv.bg>
2023-12-05 16:04:19 +0200

    Add Files

---
+++ 05.List/List-Exercise/01. Train/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int wagoncapacity = int.Parse(Console.ReadLine());
+            int number = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] tokens = command.Split();
+
+                if (tokens[0] == "Add")
+                {
+                    number = int.Parse(tokens[1]);
+                    wagons = AddWagons(wagons, number);
+                }
+                else
+                {
+                    number = int.Parse(tokens[0]);
+                    wagons = FitPassengersInWagon(wagons, number, wagoncapacity);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(" ", wagons));
+        }
+
+        static List<int> AddWagons(List<int> wagons, int number)
+        {
+            wagons.Add(number);
+
+            return wagons;
+        }
+
+        static List<int> FitPassengersInWagon(List<int> wagons, int number, int wagonsCapacity)
+        {
+            for (int i = 0; i < wagons.Count; i++)
+            {
+                if ((wagons[i] + number) <= wagonsCapacity)
+                {
+                    wagons[i] += number;
+                    break;
+                }
+            }
+
+            return wagons;
+        }
+    }
+}
---
+++ 05.List/List-Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> line1 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> line2 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int longestList = Math.Max(line1.Count, line2.Count);
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < longestList; i++)
+            {
+                if (i < line1.Count)
+                {
+                    result.Add(line1[i]);
+                }
+
+                if (i < line2.Count)
+                {
+                    result.Add(line2[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
