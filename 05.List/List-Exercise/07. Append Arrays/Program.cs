dcecbfe99aef93b5c29d1876323cd4478fb11aef
Galin <galin_3xg@abv.bg>
2023-12-06 15:18:28 +0200

    Add File

---
+++ 05.List/List-Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listToAppend = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> result = new List<string>();
+            
+            for (int i = listToAppend.Count - 1; i >= 0; i--)
+            {
+                var currentList = listToAppend[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in currentList)
+                {
+                    result.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
