256f14a9764698fcbabfb5ac7681e0cd9a2dbc80
Galin <galin_3xg@abv.bg>
2023-12-06 11:28:11 +0200

    Add Files

---
+++ 05.List/List-Exercise/05. Bomb Numbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> specialBombNumber = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] bomb = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int specialNumber = bomb[0];
+            int power = bomb[1];
+
+            int bombIndex = specialBombNumber.IndexOf(specialNumber);
+
+            while (bombIndex != -1)
+            {
+                int leftNumber = bombIndex - power;
+                int rightNumber = bombIndex + power;
+
+                if (leftNumber > 0)
+                {
+                    leftNumber = 0;
+                }
+                if (rightNumber < specialBombNumber.Count - 1)
+                {
+                    rightNumber = specialBombNumber.Count - 1;
+                }
+                specialBombNumber.RemoveRange(leftNumber, rightNumber - leftNumber + 1);
+                bombIndex = specialBombNumber.IndexOf(specialNumber);
+            }
+            int sum = 0;
+            foreach (var item in specialBombNumber)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
