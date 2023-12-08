969ce2ccf0e1f14d417c7ec3eb74fa697044d77d
Galin <galin_3xg@abv.bg>
2023-12-08 19:55:14 +0200

    Add Files

---
+++ Exam Preparation/01. Counter-Strike/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int killedEnemys = 0;
+            int thirdBattlePrice = 0;
+
+            string command = Console.ReadLine();
+            while (command != "End of battle")
+            {
+                int distanceToEnemy = int.Parse(command);
+
+                if (energy >= distanceToEnemy)
+                {
+                    killedEnemys++;
+                    energy -= distanceToEnemy;
+                    thirdBattlePrice++;
+                    if (thirdBattlePrice == 3)
+                    {
+                        energy += killedEnemys;
+                        thirdBattlePrice = 0;
+                    }
+                }
+                else if (energy <= distanceToEnemy)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {killedEnemys} won battles and {energy} energy");
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Won battles: {killedEnemys}. Energy left: {energy}");
+        }
+    }
+}
