a2a1ff509ef47e5104d736e8a2df450a9f7282ad
Galin <galin_3xg@abv.bg>
2023-12-18 17:00:10 +0200

    Add File

---
+++ 07.Associative Arrays/Associative Arrays - Lab/04.Word Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] result = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(word => word.Length % 2 == 0)
+                .ToArray();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
