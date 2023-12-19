25ef3048110e29722224965be455d3bd76d7770d
Galin <galin_3xg@abv.bg>
2023-12-19 18:21:05 +0200

    Add File

---
+++ 06.Objects-and-Classes/Objects-and-Classes-Exercise/07. Order by Age/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Order_by_Age
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>();
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] firstPersone = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = firstPersone[0];
+                string id = firstPersone[1];
+                int age = int.Parse(firstPersone[2]);
+                List<Person> existingPerson = list.FindAll(persone => persone.ID == id);
+                if (existingPerson.Count > 0)
+                {
+                    foreach (Person item in list)
+                    {
+                        if (item.ID == id)
+                        {
+                            item.Name = name;
+                            item.Age = age;
+                        }
+                    }
+                }
+                else
+                {
+                    list.Add(new Person { Name = name, ID = id, Age = age });
+                }
+                command = Console.ReadLine();
+            }
+            List<Person> orderedPersons = list.OrderBy(person => person.Age).ToList();
+            foreach (Person item in orderedPersons)
+            {
+                Console.WriteLine($"{item.Name} with ID: {item.ID} is {item.Age} years old.");
+            }
+        }
+    }
+}
