1a06d5732b168426053bcede404cf2392b155ac4
Galin <galin_3xg@abv.bg>
2023-12-28 15:51:02 +0200

    Add File

---
+++ Exam Preparation/01. SoftUni Reception/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int employe1 = int.Parse(Console.ReadLine());
+            int employe2 = int.Parse(Console.ReadLine());
+            int employe3 = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            int totalEficianciPerHour = employe1 + employe2 + employe3;
+            int hoursCount = 0;
+
+            while (studentsCount > 0)
+            {
+                hoursCount++;
+                if (hoursCount % 4 != 0)
+                {
+                    studentsCount -= totalEficianciPerHour;
+                }
+            }
+            Console.WriteLine($"Time needed: {hoursCount}h.");
+        }
+    }
+}
