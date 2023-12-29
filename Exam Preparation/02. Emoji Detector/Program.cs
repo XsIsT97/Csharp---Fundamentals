f3c46f586eb9b50c475c9c7f79b628cc3c24547b
Galin <galin_3xg@abv.bg>
2023-12-29 18:23:57 +0200

    Add file

---
+++ Exam Preparation/02. Emoji Detector/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Emoji_Detector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            long cool = input.Where(char.IsDigit)
+                .Select(digit => int.Parse(digit.ToString()))
+                .Aggregate(1, (a, b) => a * b);
+
+            Regex emojiRegex = new Regex(@"(::|\*\*)([A-Z][a-z]{2,})\1");
+            var matches = emojiRegex.Matches(input);
+
+            List<string> coolEmojies = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                string emoji = match.Groups[2].Value;
+                int coolness = emoji.Sum(ch => ch);
+
+                if (coolness >= cool)
+                {
+                    coolEmojies.Add(match.Value);
+                }
+            }
+            Console.WriteLine($"Cool threshold: {cool}");
+            Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+            foreach (var emoji in coolEmojies)
+            {
+                Console.WriteLine(emoji);
+            }
+        }
+    }
+}
