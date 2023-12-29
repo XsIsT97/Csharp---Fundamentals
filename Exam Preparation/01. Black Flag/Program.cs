cdebab72eb90674188e36f3fa02e6a4b2b6f1ab2
Galin <galin_3xg@abv.bg>
2023-12-29 18:22:43 +0200

    Add file

---
+++ Exam Preparation/01. Black Flag/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01._Black_Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double plunderForDay = double.Parse(Console.ReadLine());
+            double expectPlunder = double.Parse(Console.ReadLine());
+            double totalPlunder = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                totalPlunder += plunderForDay;
+                if (i % 3 == 0)
+                {
+                    double aditionalPlunder = plunderForDay * 0.50;
+                    totalPlunder += aditionalPlunder;
+                }
+
+                if (i % 5 == 0)
+                {;
+                    totalPlunder -= totalPlunder * 0.30;
+                }
+            }
+
+            double percentage = (totalPlunder / expectPlunder) * 100;
+
+            if (totalPlunder >= expectPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:F2} plunder gained.");
+            }
+            else
+            {
+                Console.WriteLine($"Collected only {percentage:F2}% of the plunder.");
+            }
+        }
+    }
+}
